0f1b8c30d811ceec56ac4717634f1fcdcc4ce556
gerg <gerrrrrrrg@gmail.com>
2022-11-04 13:29:46 -0400

    add comments for secondary pool

--- pkg/pool-secondary-issues/contracts/SecondaryIssuePool.sol
+++ pkg/pool-secondary-issues/contracts/SecondaryIssuePool.sol
@@ -21,7 +21,7 @@ import "@balancer-labs/v2-interfaces/contracts/vault/IGeneralPool.sol";
 import "@balancer-labs/v2-interfaces/contracts/solidity-utils/helpers/BalancerErrors.sol";
 
 
-
+// Gerg -- this does not need to be IGeneralPool. This can be IMinimalSwapInfoPool
 contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
     using Math for uint256;
     using FixedPoint for uint256;
@@ -48,8 +48,9 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
     }
 
     //security trading status
-    bool status = true;
+    bool status = true; // Gerg -- this seems to be unused
 
+    // Gerg -- private variables should have leading underscore. I'm not going to bother putting this comment in front of each private variable
     //order references
     bytes32[] private orderRefs;
 
@@ -104,6 +105,7 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
     //mapping order ref to trade reference
     mapping(bytes32 => bytes32) private tradeRefs;
 
+    // Gerg -- events should be capitalized
     event tradeReport(
         address indexed security,
         address party,
@@ -116,7 +118,7 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
         uint256 executionDate
     );
 
-
+    // Gerg -- events should be capitalized
     event bestAvailableTrades(uint256 bestUnfilledBid, uint256 bestUnfilledOffer);
 
     event Offer(address indexed security, uint256 secondaryOffer);
@@ -167,11 +169,11 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
     }
 
     function getSecurity() external view returns (address) {
-        return address(_security);
+        return address(_security); // Gerg -- this is already an address. Why are you casting?
     }
 
     function getCurrency() external view returns (address) {
-        return address(_currency);
+        return address(_currency); // Gerg -- this is already an address. Why are you casting?
     }
 
     function getSecurityOffered() external view returns (uint256) {
@@ -179,6 +181,7 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
     }
 
     function initialize() external {
+        // Gerg -- get poolId and Vault at beginning, use local memory data. Don't do repeated storage reads.
         bytes32 poolId = getPoolId();
         (IERC20[] memory tokens, , ) = getVault().getPoolTokens(poolId);
 
@@ -192,6 +195,9 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
             fromInternalBalance: false
         });
         getVault().joinPool(getPoolId(), address(this), address(this), request);
+        
+        // Gerg -- this event doesn't make sense to me. You're adding nothing to the pool other than BPT. Why are you
+        // emitting an event saying there's an offer of _security in quantity _MAX_TOKEN_BALANCE? 
         emit Offer(_security, _MAX_TOKEN_BALANCE);
     }
 
@@ -202,9 +208,10 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
         uint256 indexOut
     ) public override onlyVault(request.poolId) whenNotPaused returns (uint256) {
         uint256[] memory scalingFactors = _scalingFactors();
-        bytes32 userData = string(request.userData).stringToBytes32();
-        uint256 tradeType_length = userData.substring(0,1).stringToUint();
+        bytes32 userData = string(request.userData).stringToBytes32(); // Gerg -- why bytes -> string -> bytes32?
+        uint256 tradeType_length = userData.substring(0,1).stringToUint(); // Gerg -- you should make a standalone userdata decoder for your pool. Hard coding this here is risky if you end up changing something.
 
+        // Gerg -- you should make a standalone userdata decoder for your pool. This is risky if you end up changing something.
         Params memory params = Params({
             trade: userData.substring(1, tradeType_length + 1).stringToBytes32(),
             price: userData.substring(tradeType_length, request.userData.length).stringToUint()
@@ -222,6 +229,7 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
             } 
             else if (request.tokenOut == IERC20(_security)) {
                 uint256 postPaidCurrencyBalance = Math.add(balances[_currencyIndex], request.amount);
+                // Gerg -- you should not be modifying request.amount. If you want to use that value to derive another number, you are free to do that, but this is risky. 
                 request.amount = Math.div(postPaidCurrencyBalance, balances[_securityIndex], false);
                 (price, amount) = newOrder(request, params, "Buy", balances);
                 if (params.trade == "Market") 
@@ -232,6 +240,7 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
             request.amount = _upscale(request.amount, scalingFactors[indexOut]);
             if (request.tokenIn == IERC20(_currency)) {
                 uint256 postPaidCurrencyBalance = Math.add(balances[_currencyIndex], request.amount);
+                // Gerg -- you should not be modifying request.amount. If you want to use that value to derive another number, you are free to do that, but this is risky. 
                 request.amount = Math.div(postPaidCurrencyBalance, balances[_securityIndex], false);
                 (price, amount) = newOrder(request, params, "Buy", balances);
                 if (params.trade == "Market") 
@@ -323,8 +332,10 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
     ) private returns (uint256, uint256) {
         require(_params.trade == "Market" || _params.trade == "Limit" || _params.trade == "Stop");
         require(_order == "Buy" || _order == "Sell");
+        // Gerg -- this construction can spit out duplicate refs. If a user makes two orders within one block, their initial order will be overwritten. This is a loss of funds vulnerability. You must use an incrementing nonce here.
         bytes32 ref = keccak256(abi.encodePacked(_request.from, block.timestamp));
         //fill up order details
+        // Gerg -- you are doing 9 storage writes here. Fill in a local IOrder.order object, and then write to orders[ref] once.
         orders[ref].party = _request.from;
         orders[ref].price = _params.price;
         orders[ref].orderno = orderRefs.length;
@@ -339,22 +350,24 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
         orderRefs.push(ref);
         userOrderIndex[ref] = userOrderRefs[_request.from].length;
         userOrderRefs[_request.from].push(ref);
-        if (_params.trade == "Market") {
-            orders[ref].status = "Filled";
+        if (_params.trade == "Market") { // Gerg -- these strings should probably be enums
+            // Gerg -- why are you instantly declaring this order filled without knowing if it has been? This seems misleading at best
+            orders[ref].status = "Filled"; // Gerg -- these strings should probably be enums
             marketOrders[orders[ref].orderno] = ref;
             marketOrderbook = marketOrderbook + 1;
             return matchOrders(ref, "market");
-        } else if (_params.trade == "Limit") {
-            orders[ref].status = "Open";
+        } else if (_params.trade == "Limit") { // Gerg -- these strings should probably be enums
+            orders[ref].status = "Open"; // Gerg -- these strings should probably be enums
             limitOrders[orders[ref].orderno] = ref;
             limitOrderbook = limitOrderbook + 1;
             checkLimitOrders(_params.price);
-        } else if (_params.trade == "Stop") {
-            orders[ref].status = "Open";
+        } else if (_params.trade == "Stop") { // Gerg -- these strings should probably be enums
+            orders[ref].status = "Open"; // Gerg -- these strings should probably be enums
             stopOrders[orders[ref].orderno] = ref;
             stopOrderbook = stopOrderbook + 1;
             checkStopOrders(_params.price);
         }
+        // Gerg -- returning (0,0) for an unsuccessful trade is equivalent to stealing tokens. You must revert here instead.
         return (0, 0);
     }
 
@@ -367,7 +380,10 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
         uint256 _price,
         uint256 _qty
     ) external override {
-        require(orders[ref].status == "Open");
+        require(orders[ref].status == "Open"); // Gerg -- add revert message
+
+        // Gerg: get rid of this if block and replace with:
+        // Gerg: require(orders[ref].party == msg.sender, "Sender not allowed")
         if (orders[ref].party == msg.sender) {
             orders[ref].price = _price;
             orders[ref].qty = _qty;
@@ -401,6 +417,7 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
     function checkLimitOrders(uint256 _priceFilled) private {
         bytes32 ref;
         for (uint256 i = 0; i < limitOrderbook; i++) {
+            // Gerg -- the two blocks w/in these if/elif conditional appear identical. Is this conditional necessary?
             if (orders[limitOrders[i]].order == "Buy" && orders[limitOrders[i]].price >= _priceFilled) {
                 marketOrders[orders[limitOrders[i]].orderno] = limitOrders[i];
                 marketOrderbook = marketOrderbook + 1;
@@ -422,6 +439,7 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
     function checkStopOrders(uint256 _priceFilled) private {
         bytes32 ref;
         for (uint256 i = 0; i < stopOrderbook; i++) {
+            // Gerg -- the two blocks w/in these if/elif conditional appear identical. Is this conditional necessary?
             if (orders[stopOrders[i]].order == "Buy" && orders[stopOrders[i]].price <= _priceFilled) {
                 marketOrders[orders[stopOrders[i]].orderno] = stopOrders[i];
                 marketOrderbook = marketOrderbook + 1;
@@ -439,21 +457,22 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
     }
 
     function reorder(uint256 position, bytes32 list) private {
+        // Gerg -- shifting array elements is tremendously gas inefficient.
         if (list == "market") {
             for (uint256 i = position; i < marketOrderbook; i++) {
-                if (i == marketOrderbook - 1) delete marketOrders[position];
+                if (i == marketOrderbook - 1) delete marketOrders[position]; // Gerg -- these inline if statements are hard to read. Should use { } new newline
                 else marketOrders[position] = marketOrders[position + 1];
             }
             marketOrderbook = marketOrderbook - 1;
         } else if (list == "limit") {
             for (uint256 i = position; i < limitOrderbook; i++) {
-                if (i == limitOrderbook - 1) delete limitOrders[position];
+                if (i == limitOrderbook - 1) delete limitOrders[position]; // Gerg -- these inline if statements are hard to read. Should use { } new newline
                 else limitOrders[position] = limitOrders[position + 1];
             }
             limitOrderbook = limitOrderbook - 1;
         } else if (list == "stop") {
             for (uint256 i = position; i < stopOrderbook; i++) {
-                if (i == stopOrderbook - 1) delete stopOrders[position];
+                if (i == stopOrderbook - 1) delete stopOrders[position]; // Gerg -- these inline if statements are hard to read. Should use { } new newline
                 else stopOrders[position] = stopOrders[position + 1];
             }
             stopOrderbook = stopOrderbook - 1;
@@ -464,6 +483,10 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
     //Buyers get the best price (lowest offer) they can buy at.
     function matchOrders(bytes32 _ref, bytes32 _trade) private returns (uint256, uint256) {
 
+        // Gerg - this function should likely be broken into buy and sell sub-functions
+        // Gerg - is iterating over all orders really the best way to find your order by reference?
+        // Gerg - seems like a lot of the code here (and gas ineffiency) is a workaround for dealing with the marketOrders array. 
+        //        I know it's a declared as a mapping but the way you're using it, it's an array.
         for (uint256 i = 0; i < marketOrderbook; i++) {
             if (
                 marketOrders[i] != _ref
@@ -536,6 +559,7 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
                 checkLimitOrders(orders[_ref].price);
                 checkStopOrders(orders[_ref].price);
             }
+            // Gerg -- don't return zeros. This is equivalent to stealing trader funds
             return (0, 0);
         } else if (orders[_ref].order == "Buy") {
             if (bestOffer != "") {
@@ -581,6 +605,7 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
                 checkLimitOrders(orders[_ref].price);
                 checkStopOrders(orders[_ref].price);
             }
+            // Gerg -- don't return zeros. This is equivalent to stealing trader funds
             return (0, 0);
         }
     }
@@ -650,6 +675,7 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
         ISettlor(balancerManager).postSettlement(tradeToSettle, _tradeRef);*/
     }
 
+    // Gerg -- don't need b,a in returns if you're returning bid and ask
     function getTrade(bytes32 ref) external view override returns (uint256 b, uint256 a) {
         uint256 bid = 0;
         uint256 ask = 0;
@@ -664,13 +690,14 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
         return (bid, ask);
     }
 
+    // Gerg -- this seems like a tremendous amount of power for the manager
     function revertTrade(
         bytes32 _orderRef,
         uint256 _qty,
         bytes32 _order
     ) external override {
         require(_order == "Buy" || _order == "Sell");
-        require(balancerManager == msg.sender);
+        require(balancerManager == msg.sender); // Gerg -- this should be the first line
         orders[_orderRef].qty = orders[_orderRef].qty + _qty;
         orders[_orderRef].status = "Open";
         orders[_orderRef].orderno = orderRefs.length;
@@ -680,6 +707,7 @@ contract SecondaryIssuePool is BasePool, IGeneralPool, IOrder, ITrade {
 
     function orderFilled(bytes32 partyRef, bytes32 counterpartyRef) external override {
         require(balancerManager == msg.sender);
+        // Gerg -- are you really storing data in all these different places? Is this necessary?
         delete userOrderRefs[orders[partyRef].party][userOrderIndex[partyRef]];
         delete userOrderIndex[partyRef];
         delete orders[partyRef];
